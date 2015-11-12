35e91448467df1124eca379dc8d455f84f0816d9
Madhu Kanoor <mkanoor@redhat.com>
2015-11-12 15:53:35 -0500

    Domain Priority doesn't need a default value
    https://bugzilla.redhat.com/show_bug.cgi?id=1281582
    
    The default value for the  priority of a domain is set in the model
    MiqAeDomain#default_priority. The callers dont have to pass it in.
    This spec helper file was always setting it to 10. Which is not
    desirable for some testcases

--- spec/support/automation_spec_helper.rb
+++ spec/support/automation_spec_helper.rb
@@ -71,7 +71,6 @@ module AutomationSpecHelper
     attrs.reverse_merge!(
       :ae_class      => 'CLASS1',
       :ae_namespace  => 'A/B/C',
-      :priority      => 10,
       :enabled       => true,
       :instance_name => 'instance1')
   end
